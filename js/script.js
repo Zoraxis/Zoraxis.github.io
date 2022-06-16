92a410624846de1d1f77caaae2e1956fa69892c1
Zoraxis <b.v.o0077@gmail.com>
2022-06-16 21:50:45 +0200

    mobile support testing

--- js/script.js
+++ js/script.js
@@ -155,5 +155,123 @@ if (!isMobile) {
     });
 }
 else{
-    document.body.innerHTML = 'Hello World';
+    document.body.outerHTML = 
+    `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+	    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;700;900&display=swap" rel="stylesheet">
+    </head>
+    <style>
+
+    body {
+        margin: 0px;
+    }
+    
+    #container {
+        position: absolute;
+        margin: auto;
+        width: 100vw;
+        height: 80pt;
+        top: 0;
+        bottom: 0;
+        
+        filter: url(#threshold) blur(0.6px);
+    }
+    
+    #text1,
+    #text2 {
+        position: absolute;
+        width: 100%;
+        display: inline-block;
+    
+        font-family: "Montserrat", sans-serif;
+        font-size: 80pt;
+        font-weight: 400;
+    
+        text-align: center;
+    
+        user-select: none;
+    }
+    </style>
+
+
+
+    <body>
+        <div id="container">
+            <span id="text1"></span>
+            <span id="text2"></span>
+        </div>
+
+    <svg id="filters">
+        <defs>
+            <filter id="threshold">
+                <feColorMatrix in="SourceGraphic" type="matrix" values="1 0 0 0 0
+                    0 1 0 0 0
+                    0 0 1 0 0
+                    0 0 0 255 -140" />
+            </filter>
+        </defs>
+    </svg>
+    </body>
+
+
+
+    <script>
+    const elts = {
+        text1: document.getElementById("text1"),
+        text2: document.getElementById("text2")
+    };
+    
+    const texts = [
+        "Чё",
+        "Как",
+        "Бомж",
+        "Зайди",
+        "С",
+        "ПК",
+        "Как",
+        "Мужик",
+        ":)"
+    ];
+    
+    const morphTime = 1;
+    const cooldownTime = 0.25;
+    
+    let textIndex = texts.length - 1;
+    let time = new Date();
+    let morph = 0;
+    let cooldown = cooldownTime;
+    
+    elts.text1.textContent = texts[textIndex % texts.length];
+    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+    
+    function doMorph() {
+        morph -= cooldown;
+        cooldown = 0;
+    
+        let fraction = morph / morphTime;
+    
+        if (fraction > 1) {
+            cooldown = cooldownTime;
+            fraction = 1;
+        }
+    
+        setMorph(fraction);
+    }
+    
+    function setMorph(fraction) {
+        elts.text2.style.filter = \`blur(${Math.min(8 / fraction - 8, 100)}px)\`;
+        elts.text2.style.opacity = \`${Math.pow(fraction, 0.4) * 100}%\`;
+    
+        fraction = 1 
+    </script>
+    </html>
+    `
 }
