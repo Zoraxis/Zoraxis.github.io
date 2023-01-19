6d393a5a47e20a61227ca799c3b47340a063cffb
Zoraxis <b.v.o0077@gmail.com>
2023-01-19 22:24:51 +0100

    mobile v0.3.1

--- m/mobile.js
+++ m/mobile.js
@@ -1,5 +1,5 @@
-let coef = 0.7;
-$(".adv2-highlight").each(function () {
-    $(this).attr("width", parseFloat($(this).attr("width")) * coef);
-    $(this).attr("height", parseFloat($(this).attr("height")) * coef);
-})
+// let coef = 0.85;
+// $(".adv2-highlight").each(function () {
+//     $(this).attr("width", parseFloat($(this).attr("width")) * coef);
+//     $(this).attr("height", parseFloat($(this).attr("height")) * coef);
+// })
