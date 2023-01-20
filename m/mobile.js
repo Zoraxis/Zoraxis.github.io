b8fbfa8112b7bcd24d0eec23fda3a847fd7ed361
Zoraxis <b.v.o0077@gmail.com>
2023-01-20 18:37:06 +0100

    mobile rotate fix

--- m/mobile.js
+++ m/mobile.js
@@ -2,13 +2,16 @@ function resizeEvent(){
     if(window.orientation == 90){
         $("#landscape").fadeIn("fast")
         $("#landscape-container svg g path").attr("transform", "scale(-1,1)")
-        document.title = $("#landscape-container svg").attr("transform")
+        $(document.body).addClass("no-overflow")
     }else if (window.orientation == -90) {
         $("#landscape").fadeIn("fast")
         $("#landscape-container svg g path").attr("transform", "scale(1,1)")
-        document.title = $("#landscape-container svg").attr("transform")
+        $(document.body).addClass("no-overflow")
     }
-    else $("#landscape").fadeOut("fast")
+    else {
+        $("#landscape").fadeOut("fast")
+        $(document.body).removeClass("no-overflow")
+    }
 }
 resizeEvent()
 $(window).resize(resizeEvent)
