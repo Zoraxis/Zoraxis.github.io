df442ed57a1dcfe64490d39eb2c98fd9be18db42
Zoraxis <b.v.o0077@gmail.com>
2023-02-19 20:57:06 +0100

    latest changes applied

--- js/script.js
+++ js/script.js
@@ -1,16 +1,65 @@
+let flag = false;
+let factId = 0;
+
 $(function () {
-    $("#icon svg").css({
-        transform: `scale(${$(window).width() / 1920})`,
+    $( window ).resize(fixSize)
+    fixSize()
+
+    $("#plan-detailed-btn").bind("click touchstart", function () {
+        callFlag(() => {
+            $(".plan-full").slideToggle("fast")
+            var txt = $(this).text()
+            $(this).text($(this).data("alt"))
+                .data("alt", txt)
+        })
     })
-    $("#placeholder-icon").css({
-        transform: `scale(${$(window).width() / 1920})`,
+    $("#adv1-det").bind("click touchstart", function () {
+        callFlag(() => {
+            $(".card-extra").slideToggle(500)
+            $("#advs1-grad").fadeToggle()
+            var txt = $(this).text()
+            $(this).text($(this).data("alt"))
+                .data("alt", txt)
+        })
+    })
+    $(".fact").each(function (index) {
+        $(this).children().last().attr("id", "f" + index).data("id", index);
+    })
+    $(".fact").bind("click touchstart", function () {
+        callFlag(() => {
+            let target = $(this).children("div").last()
+            factId = target.data("id")
+            $("#fact-overlay-name").text(target.text())
+            $("#fact-overlay-txt").text(target.data("desc"))
+            $("#fact-overlay").fadeIn()
+                .next().fadeIn()
+        })
+    })
+    $("#next-fact").bind("click touchstart", function () {
+        callFlag(() => {
+            if (factId + 1 > 11) {
+                $(this).parent().fadeOut().next().fadeOut()
+            } else {
+                let target = $("#f" + (factId + 1));
+                $("#fact-overlay-name").text(target.text())
+                $("#fact-overlay-txt").text(target.data("desc"))
+                factId++;
+            }
+        })
     })
 
-    var flag = false;
-    $("#plan-detailed-btn").bind("click touchstart", function () { 
-        if(flag) return;
+    function callFlag(action) {
+        if (flag) return;
         flag = true;
-        $(".plan-full").slideToggle("fast")
-        setTimeout(function(){ flag = false; }, 300);
-    })
+        action();
+        setTimeout(function () {
+            flag = false;
+        }, 300);
+    }
+
+    function fixSize(){
+        $("#icon svg").css({
+            transform: `scale(${$(window).width() / 1920})`,
+        })
+    }
 })
